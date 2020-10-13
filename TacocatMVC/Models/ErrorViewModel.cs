3ff8e911a078b673beb022bddf857c17e71e8dcb
joshpters <joshuapetersmail@gmail.com>
2020-10-13 14:04:07 -0400

    Initial commit

---
+++ TacocatMVC/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace TacocatMVC.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
